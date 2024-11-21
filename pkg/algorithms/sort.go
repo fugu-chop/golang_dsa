a39f3958525a1b481cdb4a49b44e2dcff36190c3
Dean Wan <deanwan14@gmail.com>
2024-11-21 21:56:27 +1100

    refactor: reduce variables in  bubble sort

--- pkg/algorithms/sort.go
+++ pkg/algorithms/sort.go
@@ -8,28 +8,20 @@ func BubbleSort(arr []int) []int {
 	arrCopy := make([]int, len(arr))
 	copy(arrCopy, arr)
 
-	if len(arrCopy) == 1 {
-		return arrCopy
-	}
-
 	continueIterating := true
-	changes := 0
   end := len(arrCopy)-2
 
 	for continueIterating {
+    continueIterating = false
 		for firstMarker := 0; firstMarker <= end; firstMarker++ {
 			secondMarker := firstMarker + 1
 			if arrCopy[firstMarker] > arrCopy[secondMarker] {
 				temp := arrCopy[firstMarker]
 				arrCopy[firstMarker] = arrCopy[secondMarker]
 				arrCopy[secondMarker] = temp
-				changes++
+				continueIterating = true
 			}
 		}
-		if changes == 0 {
-			continueIterating = false
-		}
-		changes = 0
     end--
 	}
 
