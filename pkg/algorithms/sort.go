3e47bf9195890558dfaa68d442dd77df4fef19c7
Dean Wan <deanwan14@gmail.com>
2024-11-26 08:42:57 +1100

    feat: add selection sort

--- pkg/algorithms/sort.go
+++ pkg/algorithms/sort.go
@@ -22,3 +22,25 @@ func BubbleSort(arr []int) []int {
 
 	return arr
 }
+
+/*
+Selection Sort takes an unsorted slice of int and returns that slice
+with int sorted ascending (i.e. the original slice is mutated).
+*/
+func SelectionSort(arr []int) []int {
+	startIdx := 0
+	arrLen := len(arr) - 1
+
+	for startIdx < len(arr) {
+		lowestNumIdx := startIdx
+		for i := startIdx; i <= arrLen; i++ {
+			if arr[i] < arr[lowestNumIdx] {
+				lowestNumIdx = i
+			}
+		}
+		arr[startIdx], arr[lowestNumIdx] = arr[lowestNumIdx], arr[startIdx]
+		startIdx++
+	}
+
+	return arr
+}
