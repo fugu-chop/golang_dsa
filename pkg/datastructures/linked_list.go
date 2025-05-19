43ea5f44ade6a37815fca4b33c666f92cc2eec56
Dean Wan <deanwan14@gmail.com>
2025-05-20 08:23:43 +1000

    doc: add docstrings for linked list

--- pkg/datastructures/linked_list.go
+++ pkg/datastructures/linked_list.go
@@ -2,15 +2,29 @@ package datastructures
 
 import "fmt"
 
+/*
+LinkedList is an implementation of a Linked List
+data structure. It contains a pointer to a `node` type.
+*/
+
 type LinkedList struct {
 	node *node
 }
 
+/*
+The node type is intended to be used as the node
+component of LinkedList. Each node has two attributes:
+
+1. A pointer to an int; and
+
+2. A pointer to the next `node`.
+*/
 type node struct {
 	value *int
 	next  *node
 }
 
+// New returns a pointer to a new LinkedList.
 func New(val int) *LinkedList {
 	return &LinkedList{
 		node: &node{
@@ -19,6 +33,12 @@ func New(val int) *LinkedList {
 	}
 }
 
+/*
+ReadAt follows the list of nodes in the Linked List until it
+reaches the node at index `idx`.
+
+It returns an error if there is no node at index `idx`.
+*/
 func (l *LinkedList) ReadAt(idx int) (int, error) {
 	n := l.node
 	for range idx {
@@ -31,7 +51,14 @@ func (l *LinkedList) ReadAt(idx int) (int, error) {
 	return *n.value, nil
 }
 
-func (l *LinkedList) Search(value int) (int, error) {
+/*
+IndexOf searches the Linked List for a node that has a value of `value`,
+returning the 'index' of the node that contains the value.
+
+It returns an error if the `value` does not exist within any of the nodes in
+the Linked List.
+*/
+func (l *LinkedList) IndexOf(value int) (int, error) {
 	idx := 0
 	n := l.node
 
