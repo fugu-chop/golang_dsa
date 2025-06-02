db4e554d1625364be74ea84555e04feae6f23536
Dean Wan <dean@prodincident.com>
2025-06-02 13:53:38 +1000

    feat: add delete implementation

--- pkg/datastructures/binary_search_tree.go
+++ pkg/datastructures/binary_search_tree.go
@@ -1,7 +1,7 @@
 package datastructures
 
 type binarySearchTree struct {
-	currentNode *doubleLinkedNode
+	CurrentNode *doubleLinkedNode
 }
 
 /*
@@ -10,7 +10,7 @@ with the value of the currentNode set to `val`.
 */
 func BinarySearchTree(val int) *binarySearchTree {
 	return &binarySearchTree{
-		currentNode: &doubleLinkedNode{
+		CurrentNode: &doubleLinkedNode{
 			value: val,
 		},
 	}
@@ -22,8 +22,8 @@ tree contains the value of `val`. If a node does not contain `val`,
 `nil` is returned.
 */
 func (b *binarySearchTree) Search(val int, node *doubleLinkedNode) *doubleLinkedNode {
-	if node == nil || node.value == val {
-		return node
+	if node == nil {
+		return nil
 	}
 
 	if val < node.value {
@@ -34,7 +34,7 @@ func (b *binarySearchTree) Search(val int, node *doubleLinkedNode) *doubleLinked
 		return b.Search(val, node.next)
 	}
 
-	return nil
+	return node
 }
 
 /*
@@ -43,7 +43,7 @@ a doubleLinkedNode to the appropriate node based on provided `val`.
 If a node with the value of `val` already exists, Insert is a no-op.
 */
 func (b *binarySearchTree) Insert(val int, node *doubleLinkedNode) {
-	if node.value < val {
+	if val > node.value {
 		if node.next == nil {
 			node.next = &doubleLinkedNode{
 				value: val,
@@ -55,7 +55,7 @@ func (b *binarySearchTree) Insert(val int, node *doubleLinkedNode) {
 
 	// We require the additional conditional check to ensure
 	// that node.value == val is a no-op.
-	if node.value > val {
+	if val < node.value {
 		if node.prev == nil {
 			node.prev = &doubleLinkedNode{
 				value: val,
@@ -64,4 +64,45 @@ func (b *binarySearchTree) Insert(val int, node *doubleLinkedNode) {
 		}
 		b.Insert(val, node.prev)
 	}
+}
+
+/*
+Delete removes a node from the binary search tree. If the value does not
+exist within the binarySearchTree, Delete is a no-op.
+*/
+func (b *binarySearchTree) Delete(val int, node *doubleLinkedNode) *doubleLinkedNode {
+	// node with value of `val` does not exist
+	if node == nil {
+		return nil
+	}
+
+	if val < node.value {
+		node.prev = b.Delete(val, node.prev)
+	}
+
+	if val > node.value {
+		node.next = b.Delete(val, node.next)
+	}
+
+	if node.prev == nil {
+		return node.next
+	}
+
+	if node.next == nil {
+		return node.prev
+	}
+
+	node.next = b.lift(node.next, node)
+
+	return node
+}
+
+func (b *binarySearchTree) lift(node *doubleLinkedNode, deletionNode *doubleLinkedNode) *doubleLinkedNode {
+	if node.prev != nil {
+		node.prev = b.lift(node.prev, deletionNode)
+		return node
+	}
+
+	deletionNode.value = node.value
+	return node.next
 }
