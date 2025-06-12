e6d00899e1123b1b95f8f6feaba06adfaa923d40
Dean Wan <dean@prodincident.com>
2025-06-12 22:09:27 +1000

    test: add unit tests for insert, show and lastNode

---
+++ pkg/datastructures/binary_heap_test.go
@@ -0,0 +1,58 @@
+package datastructures_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/fugu-chop/golang_dsa/pkg/datastructures"
+)
+
+func TestBinaryHeap_Insert(t *testing.T) {
+	t.Parallel()
+
+	t.Run("inserts lower value nodes", func(t *testing.T) {
+		t.Parallel()
+
+		rootNode := 5
+		heap := datastructures.BinaryHeap(rootNode)
+
+		for i := 1; i < 5; i++ {
+			heap.Insert(i)
+		}
+
+		expectedHeap := []int{5, 4, 2, 1, 3}
+
+		if heap.FirstNode() != rootNode {
+			t.Fatalf("expected first node: %d, got: %d", rootNode, heap.FirstNode())
+		}
+
+		if !reflect.DeepEqual(heap.Show(), expectedHeap) {
+			t.Fatalf("expected heap: %+v, got: %+v", expectedHeap, heap.Show())
+		}
+
+	})
+
+	t.Run("replaces root node when inserted value is higher", func(t *testing.T) {
+		t.Parallel()
+		heap := datastructures.BinaryHeap(5)
+
+		heap.Insert(8)
+
+		if heap.FirstNode() != 8 {
+			t.Fatalf("expected first node: 8, got: %d", heap.FirstNode())
+		}
+
+		heap.Insert(10)
+
+		if heap.FirstNode() != 10 {
+			t.Fatalf("expected first node: 10, got: %d", heap.FirstNode())
+		}
+
+		expectedHeap := []int{10, 5, 8}
+
+		if !reflect.DeepEqual(heap.Show(), expectedHeap) {
+			t.Fatalf("expected heap: %+v, got: %+v", expectedHeap, heap.Show())
+		}
+
+	})
+}
