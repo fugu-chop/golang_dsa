339c865a668c8877988e3e362932e19ba8e80a7b
Dean Wan <dean@prodincident.com>
2025-06-30 21:27:31 +1000

    test: fix flakiness

--- pkg/datastructures/trie_test.go
+++ pkg/datastructures/trie_test.go
@@ -31,9 +31,16 @@ func TestTrie_Insert(t *testing.T) {
 		trie.Insert("car")
 
 		letters := trie.Traverse(trie.Root(), []string{})
-		expectedLetters := []string{"c", "a", "t", "*", "r", "*"}
-		if !reflect.DeepEqual(letters, expectedLetters) {
-			t.Fatalf("expected trie to contain: %v, got: %v", expectedLetters, letters)
+
+		lettersMap := make(map[string]int)
+		for _, letter := range letters {
+			lettersMap[letter] += 1
+		}
+
+		expectedMap := map[string]int{
+			"c": 1, "a": 1, "t": 1, "*": 2, "r": 1}
+		if !reflect.DeepEqual(expectedMap, lettersMap) {
+			t.Fatalf("expected trie to contain: %v, got: %v", expectedMap, lettersMap)
 		}
 	})
 
@@ -146,6 +153,32 @@ func TestTrie_Autocomplete(t *testing.T) {
 
 func TestTrie_Traverse(t *testing.T) {
 	t.Parallel()
+
+	t.Run("traverses multiple structures", func(t *testing.T) {
+		t.Parallel()
+
+		trie := datastructures.Trie()
+		trie.Insert("car")
+		trie.Insert("cartouche")
+		trie.Insert("cartographer")
+		trie.Insert("cartwheels")
+
+		letters := trie.Traverse(trie.Root(), []string{})
+
+		lettersMap := make(map[string]int)
+		for _, letter := range letters {
+			lettersMap[letter] += 1
+		}
+
+		expected := map[string]int{
+			"*": 4, "a": 2, "c": 2, "e": 4, "g": 1, "h": 3, "l": 1,
+			"o": 1, "p": 1, "r": 3, "s": 1, "t": 1, "u": 1, "w": 1,
+		}
+
+		if !reflect.DeepEqual(expected, lettersMap) {
+			t.Fatalf("expected traverse on trie to equal: %v, got: %v", expected, lettersMap)
+		}
+	})
 
 	t.Run("when root node has no children, returns", func(t *testing.T) {
 		t.Parallel()
