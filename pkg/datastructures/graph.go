e7dda2514d2f008cbb04c382cc15aaf7d98cfbc7
Dean Wan <dean@prodincident.com>
2025-07-07 08:08:56 +1000

    doc: add method notes

--- pkg/datastructures/graph.go
+++ pkg/datastructures/graph.go
@@ -2,15 +2,28 @@ package datastructures
 
 import "slices"
 
+/*
+`vertex` is a type used to implement a larger graph data structure.
+It contains a string `value` and a slice of pointers to `vertex` types
+that represent edges between other vertexes.
+*/
 type vertex struct {
 	value            string
 	adjacentVertexes []*vertex
 }
 
+/*
+AddDirectedVertex adds a single directed, unweighted edge between
+pointers to two `vertex` types.
+*/
 func (v *vertex) AddDirectedVertex(newVertex *vertex) {
 	v.adjacentVertexes = append(v.adjacentVertexes, newVertex)
 }
 
+/*
+AddUndirectedVertex adds two undirected, unweighted edges between
+pointers to two `vertex` types.
+*/
 func (v *vertex) AddUndirectedVertex(newVertex *vertex) {
 	// Avoid infinite loop
 	if slices.Contains(v.adjacentVertexes, newVertex) {
@@ -20,6 +33,7 @@ func (v *vertex) AddUndirectedVertex(newVertex *vertex) {
 	newVertex.AddUndirectedVertex(v)
 }
 
+// NewVertex creates a pointer to a `vertex` type with the value of `val`.
 func NewVertex(val string) *vertex {
 	return &vertex{
 		value:            val,
