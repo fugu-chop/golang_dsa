03e117b32dc60ab8295eb39c74cf253d95525247
Dean Wan <dean@prodincident.com>
2025-07-07 08:21:07 +1000

    doc: use correct terminology

--- pkg/datastructures/graph.go
+++ pkg/datastructures/graph.go
@@ -5,10 +5,10 @@ import "slices"
 /*
 `graph` is a type used to contain all `vertex` types within a graph.
 It enables the overall data structure to retain references to disconnected
-vertexes.
+vertices.
 */
 type graph struct {
-	vertexes map[string]*vertex
+	vertices map[string]*vertex
 }
 
 /*
@@ -16,40 +16,40 @@ NewGraph creates a pointer to a new `graph` type.
 */
 func Graph() *graph {
 	return &graph{
-		vertexes: make(map[string]*vertex),
+		vertices: make(map[string]*vertex),
 	}
 }
 
 /*
 Vertex checks to see if a pointer to a `vertex` with the value
-of `val` already exists.
+of `val` already exists within the graph.
 
   - If not, it creates a pointer to a `vertex` type with the value of `val`.
 
   - If the value already exists, a pointer to the existing `vertex` type is returned
 */
 func (g *graph) Vertex(val string) *vertex {
-	v, ok := g.vertexes[val]
+	v, ok := g.vertices[val]
 	if ok {
 		return v
 	}
 
 	newVertex := &vertex{
 		value:            val,
-		adjacentVertexes: []*vertex{},
+		adjacentVertices: []*vertex{},
 	}
-	g.vertexes[val] = newVertex
+	g.vertices[val] = newVertex
 	return newVertex
 }
 
 /*
 `vertex` is a type used to implement a larger graph data structure.
 It contains a string `value` and a slice of pointers to `vertex` types
-that represent edges between other vertexes.
+that represent edges between other vertices.
 */
 type vertex struct {
 	value            string
-	adjacentVertexes []*vertex
+	adjacentVertices []*vertex
 }
 
 /*
@@ -57,7 +57,7 @@ AddDirectedVertex adds a single directed, unweighted edge between
 pointers to two `vertex` types.
 */
 func (v *vertex) AddDirectedVertex(newVertex *vertex) {
-	v.adjacentVertexes = append(v.adjacentVertexes, newVertex)
+	v.adjacentVertices = append(v.adjacentVertices, newVertex)
 }
 
 /*
@@ -66,9 +66,9 @@ pointers to two `vertex` types.
 */
 func (v *vertex) AddUndirectedVertex(newVertex *vertex) {
 	// Avoid infinite loop
-	if slices.Contains(v.adjacentVertexes, newVertex) {
+	if slices.Contains(v.adjacentVertices, newVertex) {
 		return
 	}
-	v.adjacentVertexes = append(v.adjacentVertexes, newVertex)
+	v.adjacentVertices = append(v.adjacentVertices, newVertex)
 	newVertex.AddUndirectedVertex(v)
 }
