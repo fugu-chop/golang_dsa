e02a1bff79261e04d552c9eb3ac20a7c80c3d936
Dean Wan <dean@prodincident.com>
2025-07-07 08:17:04 +1000

    feat: add graph type

--- pkg/datastructures/graph.go
+++ pkg/datastructures/graph.go
@@ -3,6 +3,46 @@ package datastructures
 import "slices"
 
 /*
+`graph` is a type used to contain all `vertex` types within a graph.
+It enables the overall data structure to retain references to disconnected
+vertexes.
+*/
+type graph struct {
+	vertexes map[string]*vertex
+}
+
+/*
+NewGraph creates a pointer to a new `graph` type.
+*/
+func Graph() *graph {
+	return &graph{
+		vertexes: make(map[string]*vertex),
+	}
+}
+
+/*
+Vertex checks to see if a pointer to a `vertex` with the value
+of `val` already exists.
+
+  - If not, it creates a pointer to a `vertex` type with the value of `val`.
+
+  - If the value already exists, a pointer to the existing `vertex` type is returned
+*/
+func (g *graph) Vertex(val string) *vertex {
+	v, ok := g.vertexes[val]
+	if ok {
+		return v
+	}
+
+	newVertex := &vertex{
+		value:            val,
+		adjacentVertexes: []*vertex{},
+	}
+	g.vertexes[val] = newVertex
+	return newVertex
+}
+
+/*
 `vertex` is a type used to implement a larger graph data structure.
 It contains a string `value` and a slice of pointers to `vertex` types
 that represent edges between other vertexes.
@@ -31,12 +71,4 @@ func (v *vertex) AddUndirectedVertex(newVertex *vertex) {
 	}
 	v.adjacentVertexes = append(v.adjacentVertexes, newVertex)
 	newVertex.AddUndirectedVertex(v)
-}
-
-// NewVertex creates a pointer to a `vertex` type with the value of `val`.
-func NewVertex(val string) *vertex {
-	return &vertex{
-		value:            val,
-		adjacentVertexes: []*vertex{},
-	}
 }
