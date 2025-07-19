1f9948c7259353840f2b0d1dbb65c7eb1b7fb2e8
Dean Wan <dean@prodincident.com>
2025-07-19 22:32:54 +1000

    feat: add dijkstra and other search

---
+++ pkg/datastructures/weighted_graph.go
@@ -0,0 +1,254 @@
+package datastructures
+
+import (
+	"math"
+	"slices"
+)
+
+/*
+`weightedGraph` is a type used to contain all `weightedVertex` types within a graph.
+It enables the overall data structure to retain references to disconnected
+vertices.
+*/
+type weightedGraph struct {
+	vertices map[string]*weightedVertex
+}
+
+/*
+WeightedGraph creates a pointer to a new `weightedGraph` type.
+*/
+func WeightedGraph() *weightedGraph {
+	return &weightedGraph{
+		vertices: make(map[string]*weightedVertex),
+	}
+}
+
+/*
+ListVertices returns a map of all the vertices contained within
+a `weightedGraph` type, regardless of whether they are connected or not.
+*/
+func (g *weightedGraph) ListVertices() map[string]*weightedVertex {
+	return g.vertices
+}
+
+/*
+Vertex checks to see if a pointer to a `weightedVertex` with the value
+of `val` already exists within the graph.
+
+  - If not, it creates a pointer to a `weightedVertex` type with the value of `val`.
+
+  - If the value already exists, a pointer to the existing `weightedVertex` type is returned
+*/
+func (g *weightedGraph) Vertex(val string) *weightedVertex {
+	v, ok := g.vertices[val]
+	if ok {
+		return v
+	}
+
+	newVertex := &weightedVertex{
+		value:            val,
+		adjacentVertices: make(map[*weightedVertex]int),
+	}
+	g.vertices[val] = newVertex
+	return newVertex
+}
+
+/*
+`weightedVertex` is a type used to implement a weighted graph data structure.
+It contains a string `value` and a map of pointers to `weightedVertex` types
+that represent edges between other vertices and their weight.
+*/
+type weightedVertex struct {
+	value            string
+	adjacentVertices map[*weightedVertex]int
+}
+
+/*
+AddDirectedVertex adds a single directed, weighted edge between
+pointers to two `weightedVertex` types.
+*/
+func (v *weightedVertex) AddDirectedVertex(newVertex *weightedVertex, weight int) {
+	v.adjacentVertices[newVertex] = weight
+}
+
+/*
+AddUndirectedVertex adds two undirected, weighted edges between
+pointers to two `weightedVertex` types.
+*/
+func (v *weightedVertex) AddUndirectedVertex(newVertex *weightedVertex, weight int) {
+	// Avoid infinite loop
+	if _, ok := v.adjacentVertices[newVertex]; ok {
+		return
+	}
+
+	v.adjacentVertices[newVertex] = weight
+	newVertex.AddUndirectedVertex(v, weight)
+}
+
+// Value returns the underlying string value of a weightedVertex
+func (v *weightedVertex) Value() string {
+	return v.value
+}
+
+/*
+Weight returns the underlying weight of an edge between two `weightedVertex`
+types. If an edge does not exist between the two `weightedVertex`,
+#Weight returns -1
+*/
+func (v *weightedVertex) Weight(vertex *weightedVertex) int {
+	weight, ok := v.adjacentVertices[vertex]
+	if !ok {
+		// Assumption that all weights are >= 0
+		return -1
+	}
+	return weight
+}
+
+/*
+DFSWeighted performs depth-first search on a `searchVertex` against a provided `searchVal` string.
+It returns a pointer to a `weightedVertex` type if:
+
+  - A vertex exists with a value of `searchVal`; and
+
+  - That vertex has an edge to the `searchVertex`
+
+Otherwise it returns nil.
+*/
+func DFSWeighted(searchVertex *weightedVertex, searchVal string, visitedVertices map[string]bool) *weightedVertex {
+	visitedVertices[searchVertex.value] = true
+
+	if searchVertex.value == searchVal {
+		return searchVertex
+	}
+
+	for vertex := range searchVertex.adjacentVertices {
+		if visitedVertices[vertex.Value()] {
+			continue
+		}
+
+		if vertex.value == searchVal {
+			return vertex
+		}
+
+		result := DFSWeighted(vertex, searchVal, visitedVertices)
+		if result != nil && result.value == searchVal {
+			return result
+		}
+	}
+
+	return nil
+}
+
+/*
+BFSWeighted performs breadth-first search on a `searchVertex` against a provided `searchVal` string.
+It returns a pointer to a `weightedVertex` type if:
+
+  - A vertex exists with a value of `searchVal`; and
+
+  - That vertex has an edge to the `searchVertex`
+
+Otherwise it returns nil.
+*/
+func BFSWeighted(searchVertex *weightedVertex, searchVal string) *weightedVertex {
+	queue := Queue(searchVertex)
+	visitedVertices := map[string]bool{}
+
+	_ = queue.Enqueue(searchVertex)
+	visitedVertices[searchVertex.value] = true
+
+	for {
+		if _, err := queue.Read(); err != nil {
+			return nil
+		}
+
+		searchVertex, err := queue.Dequeue()
+		if err != nil {
+			return nil
+		}
+
+		if searchVertex.Value() == searchVal {
+			return searchVertex
+		}
+
+		for vertex := range searchVertex.adjacentVertices {
+			if visitedVertices[vertex.Value()] {
+				continue
+			}
+
+			if vertex.Value() == searchVal {
+				return vertex
+			}
+
+			visitedVertices[vertex.Value()] = true
+
+			_ = queue.Enqueue(vertex)
+		}
+	}
+}
+
+/*
+DijkstraShortestPath implements Dijkstra's algorithm between two `weightedVertex`
+types.
+
+It returns a slice of `weightedVertex` in the order of traversal to achieve
+the lowest possible total value of edges between `start` and `end`.
+
+If there is no sequence of edges between `start` and `end`, DijkstraShortestPath
+returns nil.
+*/
+func DijkstraShortestPath(start, end *weightedVertex) []*weightedVertex {
+	lowestCost := map[*weightedVertex]int{start: 0}
+	lowestPreviousVertex := make(map[*weightedVertex]*weightedVertex)
+	unvisitedVertices := make(map[*weightedVertex]bool)
+	visitedVertices := make(map[*weightedVertex]bool)
+	currentVertex := start
+
+	for currentVertex != nil {
+		visitedVertices[currentVertex] = true
+		delete(unvisitedVertices, currentVertex)
+
+		for nextVertex, weight := range currentVertex.adjacentVertices {
+			if _, ok := visitedVertices[nextVertex]; !ok {
+				unvisitedVertices[nextVertex] = true
+			}
+
+			weightThroughVertex := lowestCost[currentVertex] + weight
+
+			if _, ok := lowestCost[nextVertex]; !ok ||
+				weightThroughVertex < lowestCost[nextVertex] {
+				lowestCost[nextVertex] = weightThroughVertex
+				lowestPreviousVertex[nextVertex] = currentVertex
+			}
+		}
+
+		lowestWeight := math.MaxInt
+		for vertex := range unvisitedVertices {
+			weight, ok := lowestCost[vertex]
+			if ok && weight < lowestWeight {
+				lowestWeight = weight
+				currentVertex = vertex
+			}
+		}
+		// Terminate the loop if unvisitedVertices is empty
+		if lowestWeight == math.MaxInt {
+			currentVertex = nil
+		}
+	}
+
+	shortestPath := []*weightedVertex{}
+	currentVertex = end
+	for currentVertex != start {
+		shortestPath = append(shortestPath, currentVertex)
+		// Check if there is no way to get to end from start
+		result, ok := lowestPreviousVertex[currentVertex]
+		if !ok {
+			return nil
+		}
+		currentVertex = result
+	}
+
+	shortestPath = append(shortestPath, start)
+	slices.Reverse(shortestPath)
+
+	return shortestPath
+}
---
+++ pkg/datastructures/weighted_graph_test.go
@@ -0,0 +1,258 @@
+package datastructures_test
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/fugu-chop/golang_dsa/pkg/datastructures"
+)
+
+func TestWeightedVertex(t *testing.T) {
+	t.Parallel()
+
+	t.Run("creates a vertex within a graph", func(t *testing.T) {
+		t.Parallel()
+
+		g := datastructures.WeightedGraph()
+
+		_ = g.Vertex("home")
+		_ = g.Vertex("a")
+
+		got := len(g.ListVertices())
+		want := 2
+		if got != want {
+			t.Fatalf("expected graph to have %d vertices, got: %d", want, got)
+		}
+	})
+
+	t.Run("does not duplicate vertices when value is same", func(t *testing.T) {
+		g := datastructures.WeightedGraph()
+
+		_ = g.Vertex("home")
+		_ = g.Vertex("home")
+
+		got := len(g.ListVertices())
+		want := 1
+		if got != 1 {
+			t.Fatalf("expected graph to have %d vertices, got: %d", want, got)
+		}
+	})
+}
+
+func TestWeightedValue(t *testing.T) {
+	t.Parallel()
+
+	g := datastructures.WeightedGraph()
+
+	vertexValue := "home"
+	v := g.Vertex(vertexValue)
+
+	if v.Value() != vertexValue {
+		t.Fatalf("expected vertext to have value of %s, got: %s", vertexValue, v.Value())
+	}
+}
+
+func TestWeightedWeight(t *testing.T) {
+	t.Parallel()
+
+	t.Run("works for directed vertices", func(t *testing.T) {
+		t.Parallel()
+
+		g := datastructures.WeightedGraph()
+
+		v := g.Vertex("home")
+		x := g.Vertex("away")
+
+		expected := rand.Intn(10)
+		v.AddDirectedVertex(x, expected)
+
+		if v.Weight(x) != expected {
+			t.Fatalf("expected vertex to have weight of %d, got: %d", expected, v.Weight(x))
+		}
+	})
+
+	t.Run("works for undirected vertices", func(t *testing.T) {
+		t.Parallel()
+
+		g := datastructures.WeightedGraph()
+
+		v := g.Vertex("home")
+		x := g.Vertex("away")
+
+		expected := rand.Intn(10)
+		v.AddUndirectedVertex(x, expected)
+
+		if v.Weight(x) != expected {
+			t.Fatalf("expected vertex to have weight of %d, got: %d", expected, v.Weight(x))
+		}
+	})
+
+	t.Run("returns -1 if no edge exists", func(t *testing.T) {
+		t.Parallel()
+
+		g := datastructures.WeightedGraph()
+
+		v := g.Vertex("home")
+		x := g.Vertex("away")
+
+		if v.Weight(x) != -1 {
+			t.Fatalf("expected vertex to have weight of -1, got: %d", v.Weight(x))
+		}
+	})
+}
+
+func TestAddUndirectedWeightedVertex(t *testing.T) {
+	t.Parallel()
+
+	g := datastructures.WeightedGraph()
+
+	v := g.Vertex("home")
+	a := g.Vertex("a")
+	b := g.Vertex("b")
+	v.AddUndirectedVertex(a, rand.Intn(10))
+	v.AddUndirectedVertex(b, rand.Intn(10))
+
+	connectedVertexA := datastructures.DFSWeighted(v, "a", map[string]bool{})
+	if connectedVertexA == nil {
+		t.Fatal("expected home vertex to have edge to a")
+	}
+
+	connectedVertexB := datastructures.DFSWeighted(v, "b", map[string]bool{})
+	if connectedVertexB == nil {
+		t.Fatal("expected home vertex to have edge to b")
+	}
+
+	connectedVertex := datastructures.DFSWeighted(a, "home", map[string]bool{})
+	if connectedVertex == nil {
+		t.Fatal("expected vertex a to have edge to home")
+	}
+
+	connectedVertex = datastructures.DFSWeighted(b, "home", map[string]bool{})
+	if connectedVertex == nil {
+		t.Fatal("expected vertex b to have edge to home")
+	}
+
+	connectedVertex = datastructures.DFSWeighted(a, "b", map[string]bool{})
+	if connectedVertex == nil {
+		t.Fatal("expected vertex a to have edge to b")
+	}
+
+	connectedVertex = datastructures.DFSWeighted(b, "a", map[string]bool{})
+	if connectedVertex == nil {
+		t.Fatal("expected vertex b to have edge to a")
+	}
+}
+
+func TestWeightedDFS(t *testing.T) {
+	t.Parallel()
+
+	graph := datastructures.WeightedGraph()
+
+	a := graph.Vertex("a")
+	b := graph.Vertex("b")
+	c := graph.Vertex("c")
+	d := graph.Vertex("d")
+	e := graph.Vertex("e")
+	f := graph.Vertex("f")
+	g := graph.Vertex("g")
+
+	/*
+		graph should look like:
+			   a
+			/ | \ \
+		   b  d  e-g
+		  /   |  | /
+		 c-----  f/
+	*/
+
+	a.AddDirectedVertex(b, rand.Intn(10))
+	a.AddDirectedVertex(d, rand.Intn(10))
+	b.AddDirectedVertex(c, rand.Intn(10))
+	c.AddDirectedVertex(d, rand.Intn(10))
+	a.AddDirectedVertex(e, rand.Intn(10))
+	a.AddDirectedVertex(g, rand.Intn(10))
+	e.AddDirectedVertex(g, rand.Intn(10))
+	e.AddDirectedVertex(f, rand.Intn(10))
+	f.AddDirectedVertex(g, rand.Intn(10))
+
+	got := datastructures.DFSWeighted(a, "f", map[string]bool{})
+	if got != f {
+		t.Fatalf("expected to return vertex with value of: %v, got: %v", f, got)
+	}
+
+	got = datastructures.DFSWeighted(d, "f", map[string]bool{})
+	if got != nil {
+		t.Fatal("expected no vertex")
+	}
+
+	got = datastructures.DFSWeighted(d, "d", map[string]bool{})
+	if got != d {
+		t.Fatalf("expected to return vertex with value of: %v, got: %v", d, got)
+	}
+}
+
+func TestWeightedBFS(t *testing.T) {
+	t.Parallel()
+
+	graph := datastructures.WeightedGraph()
+
+	a := graph.Vertex("a")
+	b := graph.Vertex("b")
+	c := graph.Vertex("c")
+	d := graph.Vertex("d")
+	e := graph.Vertex("e")
+	f := graph.Vertex("f")
+	g := graph.Vertex("g")
+
+	/*
+		graph should look like:
+			   a
+			/ | \ \
+		   b  d  e-g
+		  /   |  | /
+		 c-----  f/
+	*/
+
+	a.AddDirectedVertex(b, rand.Intn(10))
+	a.AddDirectedVertex(d, rand.Intn(10))
+	b.AddDirectedVertex(c, rand.Intn(10))
+	c.AddDirectedVertex(d, rand.Intn(10))
+	a.AddDirectedVertex(e, rand.Intn(10))
+	a.AddDirectedVertex(g, rand.Intn(10))
+	e.AddDirectedVertex(g, rand.Intn(10))
+	e.AddDirectedVertex(f, rand.Intn(10))
+	f.AddDirectedVertex(g, rand.Intn(10))
+
+	got := datastructures.BFSWeighted(a, "f")
+	if got != f {
+		t.Fatalf("expected to return vertex with value of: %v, got: %v", f, got)
+	}
+
+	got = datastructures.BFSWeighted(d, "f")
+	if got != nil {
+		t.Fatal("expected no vertex")
+	}
+
+	got = datastructures.BFSWeighted(d, "d")
+	if got != d {
+		t.Fatalf("expected to return vertex with value of: %v, got: %v", d, got)
+	}
+}
+
+func TestDijkstraShortestPath(t *testing.T) {
+	t.Parallel()
+
+	t.Run("returns nil if no edge exists", func(t *testing.T) {
+		t.Parallel()
+
+		graph := datastructures.WeightedGraph()
+
+		a := graph.Vertex("a")
+		b := graph.Vertex("b")
+
+		result := datastructures.DijkstraShortestPath(a, b)
+		if result != nil {
+			t.Fatalf("expected no result from unconnected vertices, got: %v", result)
+		}
+	})
+}
